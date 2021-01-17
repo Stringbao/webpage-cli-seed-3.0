53c4fc8fad949a3b53ac33d94fe709cdf108fdb3
string_bao <string_bao@163.com>
2021-01-17 21:59:22 +0800

    3

--- src/origin/entry/pc/pcCheckout.js
+++ src/origin/entry/pc/pcCheckout.js
@@ -1,11 +1,9 @@
 import "@css/pc/pcRegister.scss";
 import Engine from "@src/main/Engine.js";
-import PCRegister from "@vpc/Register.js";
 
 //develpoment环境下需要执行该方法, 生产环境无需执行
 if(!$PRODUCTION){
-    // _engine.init(flash_fe_core_tool.$CONSTANT.TERMINAL.PC, $(".rectangle-copy"));
-    Engine.show(flash_fe_core_tool.$CONSTANT.TERMINAL.PC, new PCRegister($(".rectangle-copy")));
+    Engine.init(flash_fe_core_tool.$CONSTANT.TERMINAL.PC);
 }
 
 export default Engine;
